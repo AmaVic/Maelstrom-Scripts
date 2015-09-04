8798c986568bd04dc0dcb2c4bf7d8bd71de69109
Victor Amaral de Sousa <victor.amaral.ds@gmail.com>
2015-09-04 12:47:25 +0200

    Moved PvP kills handling to KillstreakManager class

--- Killstreak/script_killstreak_handling.cpp
+++ Killstreak/script_killstreak_handling.cpp
@@ -1,5 +1,6 @@
 #include "ScriptMgr.h"
 #include "KillstreakManager.h"
+#include <thread>
 
 class script_killstreak_handling : public PlayerScript {
 
@@ -10,30 +11,7 @@ public:
 	//================================================================================================================================================================
 
 	// Overriding script functions ===================================================================================================================================
-	void OnPVPKill(Player* killer, Player* killed) {
-			try {
-				//Don't modify the two following instructions
-				Maelstrom::sKillstreakMgr->InsertNewPlayer(killer);
-				Maelstrom::sKillstreakMgr->InsertNewPlayer(killed);
-
-				//We increase the killer's killstreak
-				//If you want to do it under certain conditions only, wrap the two following instructions in a "if" block
-				//For instance:
-				//if(killer->GetAreaId() == 720) {
-				// Maelstrom::sKillstreakMgr->IncrementPlayerKS(killer);
-				// Maelstrom::sKillstreakMgr->ResetPlayerKS(killed);
-				// } -> it will only count kills and reset killstreaks if the killer is in area 720
-
-				Maelstrom::sKillstreakMgr->IncrementPlayerKS(killer);
-				//We reset the killed's killstreak
-				Maelstrom::sKillstreakMgr->ResetPlayerKS(killed);
-			}
-			catch (const std::exception& e) {
-				std::cout << e.what() << std::endl;
-			}
-	}
-
-	void OnLogout(Player* player) {
+	void OnLogout(Player* player) { 
 		Maelstrom::sKillstreakMgr->RemovePlayer(player);
 	}
 	//================================================================================================================================================================
